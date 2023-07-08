dc709242c29482d24707c1ee0fd2d41a339e95f0
mohadang <ehdlaos@gmail.com>
2023-07-08 19:56:46 +0900

    POST API 응답값 변경
    - 201 OK 반환
    - 응답 헤더의 Location에 추가된 데이터를 조회할 수 있는 URI 설정

--- src/main/java/mycompany/example/hellospringboot/user/UserController.java
+++ src/main/java/mycompany/example/hellospringboot/user/UserController.java
@@ -1,7 +1,10 @@
 package mycompany.example.hellospringboot.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,8 +24,17 @@ public class UserController {
         return service.findOne(id);
     }
 
+
     @PostMapping("/users")
-    public  void createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        // ResponseEntit를 사용하여 API 호출 결과에 대한 적절한 반환을 하도록 처리
+        return ResponseEntity.created(location).build();
     }
 }
