89c1907614d89e1a2b625e2e0ec9e7c5ae4a05e1
mohadang <ehdlaos@gmail.com>
2023-07-08 19:40:20 +0900

    사용자 추가 POST REST API 추가

--- src/main/java/mycompany/example/hellospringboot/user/UserController.java
+++ src/main/java/mycompany/example/hellospringboot/user/UserController.java
@@ -1,8 +1,6 @@
 package mycompany.example.hellospringboot.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,10 @@ public class UserController {
     @GetMapping("/users/{id}")
     public User retieveAllUser(@PathVariable int id /*int 로 컨버팅됨*/) {
         return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public  void createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
     }
 }
