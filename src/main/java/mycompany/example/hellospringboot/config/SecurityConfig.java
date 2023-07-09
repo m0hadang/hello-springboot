c9a7918c2a2dbeaea422a8254555821ca478d358
mohadang <ehdlaos@gmail.com>
2023-07-09 21:02:20 +0900

    JPA 사용 환경 구성

--- src/main/java/mycompany/example/hellospringboot/config/SecurityConfig.java
+++ src/main/java/mycompany/example/hellospringboot/config/SecurityConfig.java
@@ -3,10 +3,20 @@ package mycompany.example.hellospringboot.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // h2 console 로그인시 인증 없이 로그인 할 수 있도록 처리
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();// 어떤 요청이든 허용
+        http.csrf().disable();// csrf 비활성화
+        http.headers().frameOptions().disable();
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
