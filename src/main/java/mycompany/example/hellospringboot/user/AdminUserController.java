8bd54e369bb0aed1c8ffb01c41932831a65f741a
mohadang <ehdlaos@gmail.com>
2023-07-09 12:29:13 +0900

    관리자 사용자 REST API v1, v2로 관리

--- src/main/java/mycompany/example/hellospringboot/user/AdminUserController.java
+++ src/main/java/mycompany/example/hellospringboot/user/AdminUserController.java
@@ -3,6 +3,8 @@ package mycompany.example.hellospringboot.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -39,8 +41,8 @@ public class AdminUserController {
     }
 
     // GET /users/1 or /users/10 -> String 이다
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retieveAllUser(@PathVariable int id /*int 로 컨버팅됨*/) {
+    @GetMapping("/v1/users/{id}")
+    public MappingJacksonValue retieveUserV1(@PathVariable int id /*int 로 컨버팅됨*/) {
         User user = service.findOne(id);
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
@@ -55,6 +57,28 @@ public class AdminUserController {
                 new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+    @GetMapping("/v2/users/{id}")
+    public MappingJacksonValue retieveUserV2(@PathVariable int id /*int 로 컨버팅됨*/) {
+        User user = service.findOne(id);
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);// 필드값 복사
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "grade");
+
+        FilterProvider filters =
+                new SimpleFilterProvider().addFilter("UserInfoV2", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(userV2);
         mapping.setFilters(filters);
 
         return mapping;
